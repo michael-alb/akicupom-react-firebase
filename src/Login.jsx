d66cb63159b24fbf01e195871c22fd7debc8c364
adrianomoda <adrianomodafeitosa@gmail.com>
2017-11-29 18:17:58 -0300

    Add botao Login rede social Google

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import firebase from 'firebase'
+import css from './css/Login.css'
 
 import HeaderInterno from './HeaderInterno'
 
 class Login extends Component {
+
+    
     render() {
+
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().languageCode = 'pt';
+        provider.setCustomParameters({
+        'login_hint': 'user@example.com'
+        });        
+
+        function signIn(){
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                // ...
+                }).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+                });
+        }
+
         return (
             <div>
                 <HeaderInterno />
@@ -24,6 +54,7 @@ class Login extends Component {
                     <button className='btn btn-warning' type='button'>Entrar</button>
                     <hr/>
                     <p>ou faça o login com:</p>
+                    <button className="loginBtn loginBtn--google" onClick={signIn} type='button'>Google Login</button>
                 </form>
                 </div>
                 <div className='col-lg-4'></div>
